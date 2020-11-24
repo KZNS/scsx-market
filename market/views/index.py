7eb1c898f79be090506f18ab98e42163f5283607
Nathan <tzy15368@outlook.com>
2020-11-24 15:12:49 +0800

    feat: login&register js, logout enabled

--- market/views/index.py
+++ market/views/index.py
@@ -1,6 +1,11 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,url_for,session,redirect
 index = Blueprint("index",__name__,template_folder='templates',static_folder='static')
 
 @index.route("/")
 def helo():
-    return render_template('index.html')
+    return render_template('index.html',login_url = url_for('user.log'),register_url = url_for('user.reg'))
+
+@index.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index.helo'))
