4a8176cd6fa9eab18bd39093abb36dd72beb9735
Nathan <tzy15368@outlook.com>
2020-11-24 14:36:28 +0800

    feat: add register & login backend

--- market/views/user.py
+++ market/views/user.py
@@ -13,13 +13,26 @@ def log():
     if user is not None:
         session['level'] = user.level
         session['id'] = user.id
-        return jsonify({"success":True})
+        return jsonify({"success":True,"data":"登录成功"})
     else:
-        return jsonify({"success":False})
+        return jsonify({"success":False,"details":"用户名或密码错误"})
 
 
 @user.route('/register',methods=['POST'])
 def reg():
     jsoninput = request.get_json()
-    _u = MarketStaff()
-    return '1'
+    _u = MarketStaff(
+        name = jsoninput.get('name'),
+        password_hash = hash_password(jsoninput.get('password')),
+        telephone = jsoninput.get('telephone'),
+        salary = jsoninput.get('salary'),
+    )
+    db.session.add(_u)
+    try:
+        db.session.commit()
+        return jsonify({"success":True,"data":"注册成功"})
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        return jsonify({"success":False,"details":"该电话号码已存在"})
+    
