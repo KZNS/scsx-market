18ff2601b64fc01e9d1219b647cc86e877506dba
W-RJ <wang-rj@outlook.com>
2020-11-25 10:58:16 +0800

    feat: add add to staff

--- market/views/staff.py
+++ market/views/staff.py
@@ -31,6 +31,32 @@ def staff_query():
     return render_template('staff.html', data=data)
 
 
+@staff.route("/staff/add", methods=['GET', 'POST'])
+def staff_add():
+    if request.method == 'GET':
+        return render_template('staff_add.html')
+    if request.method == 'POST':
+        item = MarketStaff(
+            staff_id=request.form.get('staff_id'),
+            name=request.form.get('name'),
+            password_hash=request.form.get('password'),
+            level=request.form.get('level'),
+            telephone=request.form.get('telephone'),
+            salary=request.form.get('salary'),
+            comment=request.form.get('comment')
+        )
+        print(item)
+        db.session.add(item)
+        try:
+            db.session.commit()
+            flash('增加成功')
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            flash('提交失败')
+        return redirect(url_for('staff.staff_query', timeout=True))
+
+
 @staff.route("/staff/del/<int:id>", methods=['DELETE'])
 def delete(id):
     MarketStaff.query.filter(MarketStaff.id == id).update({'delete': True})
