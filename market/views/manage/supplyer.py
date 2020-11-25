19196ee05d392ecf1405e8e22b7e6c29ef15a00c
Nathan <tzy15368@outlook.com>
2020-11-25 16:43:46 +0800

    upd

--- market/views/manage/supplyer.py
+++ market/views/manage/supplyer.py
@@ -1,16 +1,36 @@
 from market.views.manage import manage
-from flask import render_template,g,jsonify,redirect,url_for
+from flask import render_template,g,jsonify,redirect,url_for,request
 from market.models import db,MarketSupplyer
 from faker import Faker
 f=Faker(locale='zh_CN')
 @manage.route("/supplyerinfo",methods=['GET','POST','PUT'])
 def supplyer_home():
-    suppliers = MarketSupplyer.query.all()
-    result = []
-    for s in suppliers:
-        result.append(s.todict())
-    return jsonify(result)
-
+    if request.method=='GET':
+        suppliers = MarketSupplyer.query.all()
+        result = []
+        for s in suppliers:
+            result.append(s.todict())
+        return jsonify(result)
+    elif request.method=='POST':
+        _m = MarketSupplyer(
+            supplyer_id=request.form.get('supplyerid'),
+            name=request.form.get('supplyername'),
+            name_short=request.form.get('supplyerabbr'),
+            address=request.form.get('supplyeraddress'),
+            telephone=request.form.get('supplyerphone'),
+            email=request.form.get('supplyeremail'),
+            contact=request.form.get('supplyerperson'),
+            contact_phone=request.form.get('supplyerperson-phone'),
+            comment=request.form.get('supplyerremarks'),
+        )
+        db.session.add(_m)
+        try:
+            db.session.commit()
+            return jsonify({"success":True})
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return jsonify({"success":False})
 @manage.route('/test')
 def test():
     return render_template('test.html',title='bruh')
